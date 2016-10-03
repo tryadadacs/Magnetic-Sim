4205322324113e479c3347b902aa6c47d2e94c47
tryadadacs <emw0041@auburn.edu>
2016-10-03 09:07:51 -0500

    Add files via upload

--- determine_magnetic_field.m
+++ determine_magnetic_field.m
@@ -1,36 +1,43 @@
-clear, clc
+clc, clear all
 format bank
 tic %Initiate runtime counter
+
+% ***INPUTS***
 sim_time = 283; %minutes in simulation
     %95.63 = 1 orbit
 time_step = 10; %seconds between data sample
     %DO NOT SET BELOW 0.01 LEST YOU WISH TO MEET THE KRAKEN
-
 orb_alt = 550; %mean altitude of orbit, km
-g_parameter = 3.986E14; %graviational parameter of earth, m^3 / s^2
-earth_radius = 6378; %radius of earth, km
-semi_major = orb_alt + earth_radius; %semi_major axis height, km 
-B_field_orb = []; %Define an empty matrix to store orbital components
-
-orb_vel = sqrt(g_parameter/(semi_major * 1000)); %orbital velocity, m/s
-
-init_quat = [semi_major 0 0 0]'; %initial measured quaternion
-init_sat_ang_vel = orb_vel/sqrt(2); %initial satellite angular velocity
-init_wheel_ang_vel = orb_vel/sqrt(2); %initial reaction wheel angular velocity
-
 init_pos = [0 0]; %initial position, [latitude longitude]
 inclin = 0; %orbital inclination, degrees
     %0 < inclin < 89 means the satellite rotates with earth
     %91 < inclin < 179 means the satellite rotates against earth
 eccent = 0; %eccentricity of orbit, 0 is circular
-
-period = (2*pi*(semi_major * 1000)) / orb_vel;
-num_orb = (sim_time * 60) / period;
-fprintf('Simulating %2.2f Orbits \n', num_orb)
-
 RA_ascend = 0; %right ascension of ascending node
 arg_per   = 0; %argument of perigee
 true_anom = 0;  %true anomaly of departure
+
+% ***CONSTANTS***
+g_parameter = 3.986E14; %graviational parameter of earth, m^3 / s^2
+earth_radius = 6378; %radius of earth, km
+semi_major = orb_alt + earth_radius; %semi_major axis height, km 
+B_field_orb = []; %Define an empty matrix to store orbital components
+secs_per_day = 86164; %seconds in a solar day
+
+% ***CALCULATE***
+orb_vel = sqrt(g_parameter/(semi_major * 1000)); %orbital velocity, m/s
+init_quat = [semi_major 0 0 0]'; %initial measured quaternion
+init_sat_ang_vel = orb_vel/sqrt(2); %initial satellite angular velocity
+init_wheel_ang_vel = orb_vel/sqrt(2); %initial reaction wheel angular vel.
+period = (2*pi*(semi_major * 1000)) / orb_vel;
+num_orb = (sim_time * 60) / period;
+fprintf('Simulating %2.2f Orbits \n', num_orb)
+time_of_day = mod(time, secs_per_day); % seconds in current day
+gha = (time_of_day)/secs_per_day*360; %greenwich hour angle, degrees
+time = datenum(datetime);
+% Find the latitude and longitude from orbit specs
+[lat, long] = Orbit3D(RA_ascend, arg_per, true_anom, inclin, semi_major,...
+    eccent, time_step, num_orb);
 
 %{
 %Calculate GMSTo
@@ -49,21 +56,21 @@ GMSTo = mod(6.697374558 + 0.06570982441908.*D0  + 1.00273790935.*H + ...
     0.000026.*(T.^2),24).*15;
 %}
 
-[lat, long] = Orbit3D(RA_ascend, arg_per, true_anom, inclin, semi_major, eccent, time_step, num_orb);
-
-time = datenum(datetime);
+% Call IGRF to create a matrix of magnetic field values in the NED frame
 [Bx_NED, By_NED, Bz_NED] = igrf(time, lat, long, orb_alt);
 B_field_NED = [Bx_NED, By_NED, Bz_NED];
+
+% Convert magnetic field vector from NED to orbital frame, row by row
 NED_matrix_row = 1; %initialize a counter for the following for loop
-% Iterate through, creating a matrix of mag field components in orbital frame
+
+% Creates a matrix of mag field components in orbital frame
 for i = 0:(time_step/60):sim_time
     % Pass the value of i in seconds to the ned_to_orb function
     ned_orb_matrix = ned_to_orb(inclin, period, i*60);
-    % Gives the individual vectors for each timestep
+    % Gives the individual vectors for each timestep, and adds them to
+    % larger matrix
     rotated_B_vec = ned_orb_matrix * B_field_NED(NED_matrix_row,:)';
-    % Creates the matrix of orbital components, adding a row each iteration
     B_field_orb = [B_field_orb; rotated_B_vec'];
-    % Update the row counter so the matrix multiplication updates each time
     NED_matrix_row = NED_matrix_row + 1;
 end
 
@@ -83,19 +90,14 @@ title('Magnetic Field (orbital frame)')
 legend('Bx', 'By', 'Bz')
 xlabel('time, minutes')
 ylabel('nano-Tesla')
-
-disp(B_field_NED(1,:))
-disp(B_field_orb(1,:))
+disp(ned_orb_matrix)
 
 runtime = toc; %end runtime counter
 fprintf('Total Simulation Time = %4.2f \n', runtime)
-
-secs_per_day = 86164; %seconds in a solar day
-time_of_day = mod(time, secs_per_day); % seconds in current day
-gha = (time_of_day)/secs_per_day*360; %greenwich hour angle, degrees
 
 %NED to ECI dcm
 %vern_ang = long + gha; %angle between vernal equinox and current point
 %dcm = [-sind(lat)*cosd(vern_ang) -sind(lat)*sind(vern_ang)  cosd(lat);
 %       -sind(vern_ang)            cosd(vern_ang)                    0;
-%       -cosd(lat)*cos(vern_ang)  -cosd(lat)*sind(vern_ang) -sind(lat)]; %direction cosine matrix
+%       -cosd(lat)*cos(vern_ang)  -cosd(lat)*sind(vern_ang) -sind(lat)];...
+%direction cosine matrix
